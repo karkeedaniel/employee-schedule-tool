44b98068c6f4739ac1b9204ae4ffdc0fdd882988
karkeedaniel <karkee.daniel@gmail.com>
2016-04-19 19:02:01 -0700

    Fix to sort by end time.

--- src/main/webapp/resources/est/js/schedule.js
+++ src/main/webapp/resources/est/js/schedule.js
@@ -76,7 +76,7 @@ angular.module("estApp")
         };
 
         $scope.dtOptions = DTOptionsBuilder.newOptions()
-            .withOption('order', [2, 'asc']);
+            .withOption('order', [3, 'asc']);
         $scope.dtColumnDefs = [
             DTColumnDefBuilder.newColumnDef(6).notSortable(),
             DTColumnDefBuilder.newColumnDef(7).notSortable()
